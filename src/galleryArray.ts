b3d657c3d56d0a9448abb705105807731aa5289a
CeciliaL1 <cecilia.lepik@hotmail.com>
2024-01-03 20:58:59 +0100

    images

--- src/galleryArray.ts
+++ src/galleryArray.ts
@@ -1,50 +1,50 @@
 export const galleryArray = [
     {
-        src: 'src/images/disaplay-beige-2.jpg',
+        src: '/src/images/disaplay-beige-2.jpg',
         alt: 'abstract beige painting'
     },
     {
-        src: 'src/images/display- swirl-2.jpg',
+        src: '/src/images/display- swirl-2.jpg',
         alt: 'close up on abstract painting'
     },
     {
-        src: 'src/images/display-black.jpg',
+        src: '/src/images/display-black.jpg',
         alt: 'black abstract painting'
     },
     {
-        src: 'src/images/display-blue.jpg',
+        src: '/src/images/display-blue.jpg',
         alt: 'blue and white abstract painting'
     },
     {
-        src: 'src/images/display-flower.jpg',
+        src: '/src/images/display-flower.jpg',
         alt: 'abstract flower'
     },
     {
-        src: 'src/images/display-green.jpg',
+        src: '/src/images/display-green.jpg',
         alt: 'abstract beige and green'
     },
     {
-        src:'src/images/display-green-forest.jpg',
+        src:'/src/images/display-green-forest.jpg',
         alt: 'green forest abstract painting'
     },
     {
-        src: 'src/images/display-purple.jpg',
+        src: '/src/images/display-purple.jpg',
         alt: 'purple abstract painting'
     },
     {
-        src: 'src/images/display-rust2.jpg',
+        src: '/src/images/display-rust2.jpg',
         alt: 'rust colored abstract painting'
     },
     {
-        src: 'src/images/display-grey-rust2.jpg',
+        src: '/src/images/display-grey-rust2.jpg',
         alt: ' rust and grey abtracts painting'
     },
     {
-        src: 'src/images/display-white-black.jpg',
+        src: '/src/images/display-white-black.jpg',
         alt: 'white and black standing abstract painting'
     },
     {
-        src: 'src/images/display-red2.jpg',
+        src: '/src/images/display-red2.jpg',
         alt: 'red abstract painting'
     }
     
